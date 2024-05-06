474a7d20a5babfb79cd5ef026e84ccd58ce90ff9
Ivan-Krul <kiwiivanya128@gmail.com>
2024-05-06 19:50:18 +0200

    fixed bad vs configuration and made build.sh

---
+++ build.sh
@@ -0,0 +1,22 @@
+dest="bin"
+
+debug=""
+
+read -p "is it for 32-bit architecture?(Y/n): " $debug 
+
+mkdir -p "bin"
+
+if [[ $debug == "Y" ]]; then
+    g++ -m32 -std=c++17 "GraphEditor/GraphEditor.cpp" -o "$dest/GraphEditor"
+else
+    g++ -std=c++17 "GraphEditor/GraphEditor.cpp" -o "$dest/GraphEditor"
+fi
+
+python_files=$(find GraphEditor -type f -name "*.py")
+
+for file in $python_files; do
+    filename=$(basename "$file")
+    cp "$file" "$dest/$filename"
+done
+
+echo "done"
